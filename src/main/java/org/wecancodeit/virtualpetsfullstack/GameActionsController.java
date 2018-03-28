ab48f5a13d0cf246733b6cf3d95b71c4fc82ee60
marcbgold <marcbgold2@gmail.com>
2018-03-28 19:01:14 -0400

    added controller method for admitting new pet to shelter

--- src/main/java/org/wecancodeit/virtualpetsfullstack/GameActionsController.java
+++ src/main/java/org/wecancodeit/virtualpetsfullstack/GameActionsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class GameUpdatesController {
+public class GameActionsController {
 
 	@Resource
 	VirtualPetShelterRepository shelterRepo;
@@ -31,56 +31,56 @@ public class GameUpdatesController {
 	}
 
 	@RequestMapping(path = "/put-out-food", method = RequestMethod.PUT)
-	public String putOutFood(Model model) {
+	public String putOutFood() {
 		VirtualPetShelter shelter = shelterRepo.findOne(1L);
 		shelter.putOutFood();
 		return "redirect:/end-of-round";
 	}
 
 	@RequestMapping(path = "/put-out-water", method = RequestMethod.PUT)
-	public String putOutWater(Model model) {
+	public String putOutWater() {
 		VirtualPetShelter shelter = shelterRepo.findOne(1L);
 		shelter.putOutWater();
 		return "redirect:/end-of-round";
 	}
 
 	@RequestMapping(path = "/clean-all-cages", method = RequestMethod.PUT)
-	public String cleanAllCages(Model model) {
+	public String cleanAllCages() {
 		VirtualPetShelter shelter = shelterRepo.findOne(1L);
 		shelter.cleanAllCages();
 		return "redirect:/end-of-round";
 	}
 
 	@RequestMapping(path = "/scoop-out-litter-boxes", method = RequestMethod.PUT)
-	public String scoopOutLitterBoxes(Model model) {
+	public String scoopOutLitterBoxes() {
 		VirtualPetShelter shelter = shelterRepo.findOne(1L);
 		shelter.scoopOutLitterBoxes();
 		return "redirect:/end-of-round";
 	}
 
 	@RequestMapping(path = "/clean-floor", method = RequestMethod.PUT)
-	public String cleanFloor(Model model) {
+	public String cleanFloor() {
 		VirtualPetShelter shelter = shelterRepo.findOne(1L);
 		shelter.cleanFloor();
 		return "redirect:/end-of-round";
 	}
 
 	@RequestMapping(path = "/walk-all-dogs", method = RequestMethod.PUT)
-	public String walkAllDogs(Model model) {
+	public String walkAllDogs() {
 		VirtualPetShelter shelter = shelterRepo.findOne(1L);
 		shelter.walkAllDogs();
 		return "redirect:/end-of-round";
 	}
 
 	@RequestMapping(path = "/oil-all-robots", method = RequestMethod.PUT)
-	public String oilAllRobots(Model model) {
+	public String oilAllRobots() {
 		VirtualPetShelter shelter = shelterRepo.findOne(1L);
 		shelter.oilAllRobots();
 		return "redirect:/end-of-round";
 	}
 
 	@RequestMapping(path = "/charge-all-robots", method = RequestMethod.PUT)
-	public String chargeAllRobots(Model model) {
+	public String chargeAllRobots() {
 		VirtualPetShelter shelter = shelterRepo.findOne(1L);
 		shelter.chargeAllRobots();
 		return "redirect:/end-of-round";
---
+++ src/main/java/org/wecancodeit/virtualpetsfullstack/RosterManagementController.java
@@ -0,0 +1,45 @@
+package org.wecancodeit.virtualpetsfullstack;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class RosterManagementController {
+
+	@Resource
+	VirtualPetShelterRepository shelterRepo;
+
+	@Resource
+	VirtualPetRepository petRepo;
+
+	@Resource
+	CageRepository cageRepo;
+
+	@RequestMapping(path = "/admit-new-pet/{name}/{description}/{type}", method = RequestMethod.POST)
+	public String admitNewPet(@PathVariable String name, @PathVariable String description, @PathVariable String type) {
+		VirtualPetShelter shelter = shelterRepo.findOne(1L);
+		switch (type) {
+		case "organic-dog":
+			VirtualPet orgDog = petRepo.save(new OrganicDog(shelter, name, description));
+			cageRepo.save(new Cage(shelter, orgDog));
+			break;
+		case "robot-dog":
+			VirtualPet roboDog = petRepo.save(new RobotDog(shelter, name, description));
+			cageRepo.save(new Cage(shelter, roboDog));
+			break;
+		case "organic-cat":
+			petRepo.save(new OrganicCat(shelter, name, description));
+			break;
+		case "robot-cat":
+			petRepo.save(new RobotCat(shelter, name, description));
+			break;
+		}
+
+		return "redirect:/end-of-round";
+	}
+
+}
